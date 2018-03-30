0771b2b64ba9ac4c098558b42e773adb8685f15a
Jacob Mendelowitz <jmdev37@gmail.com>
2018-03-30 09:52:39 -0400

    Automatic date/time checking to get the correct upcoming courses.

--- project2/src/component/BaseLookupMethod.js
+++ project2/src/component/BaseLookupMethod.js
@@ -2,15 +2,33 @@ import React from "react";
 import Constants from "../data/Constants.js";
 
 class BaseLookupMethod extends React.Component {
+  getLookupSemester() {
+    const date = new Date();
+    return (date.getMonth() < 7) ? "Fall" : "Spring";
+  }
+
+  getLookupYear() {
+    const date = new Date();
+    if (date.getMonth() < 7) {
+      return date.getFullYear();
+    } else {
+      return date.getFullYear() + 1;
+    }
+  }
+
   getCoursesByQuery(query) {
-    return fetch(Constants.SERVER_URL + "q?" + query + "&Semester=Fall&Year=2018&limit=1000")
+    const semester = this.getLookupSemester();
+    const year = this.getLookupYear();
+    return fetch(Constants.SERVER_URL + "q?" + query + `&Semester=${semester}&Year=${year}&limit=1000`)
       .then((response) => response.json());
   }
 
   getCourseByID(courseID) {
+    const semester = this.getLookupSemester();
+    const year = this.getLookupYear();
     const department = courseID.slice(0, 4).toUpperCase();
     const courseNum = courseID.substring(4).trim().toUpperCase();
-    const query = `Department=${department}&CrseNum=${courseNum}&Semester=Fall&Year=2018&limit=100`;
+    const query = `Department=${department}&CrseNum=${courseNum}&Semester=${semester}&Year=${year}&limit=100`;
     return fetch(Constants.SERVER_URL + "q?" + query)
       .then((response) => response.json());
   }
